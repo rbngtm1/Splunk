ecee22ec1866e8d825ce85edcf6f9e484d5d1aa8
robin <rbngtm1@gmail.com>
2020-01-13 00:16:11 -0600

    updated roles

---
+++ roles/files/getsecretskey_splunk.sh
@@ -0,0 +1,2 @@
+# use this if you have certificates in splunk server and add it in aws secrects manager service.
+# use simple aws command to retrive that certificate and write script to write to a file.
